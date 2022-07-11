fe1a54233ee9e47e117890c8cb17623ba06cef7d
jestxfot <87380272+jestxfot@users.noreply.github.com>
2022-07-11 12:34:51 +0300

    Update URLBlocklist.ps1

--- URLBlocklist.ps1
+++ URLBlocklist.ps1
@@ -1,1 +1,1 @@
-reg add "HKEY_LOCAL_MACHINE\Software\Policies\Mozilla\Firefox\WebsiteFilter\Block" /v 1 /t REG_SZ /d **://*.xvideos.com/* /f
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Mozilla\Firefox\WebsiteFilter\Block" /v 1 /t REG_SZ /d *://*.xvideos.com/* /f
